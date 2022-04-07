e79108a9fc07fdf241db1eb9719392db744a5231
Zhihui Du <zhihuidu@gmail.com>
2022-04-07 16:53:26 -0400

    add the shutdown script

---
+++ test/client/benchmarks/shutdown.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3                                                         
+
+import time, argparse
+import numpy as np
+import arkouda as ak
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="For shutdown the arkouda server")
+    parser.add_argument('hostname', help='Hostname of arkouda server')
+    parser.add_argument('port', type=int, help='Port of arkouda server')
+    return parser
+
+
+    
+if __name__ == "__main__":
+    import sys
+    parser = create_parser()
+    args = parser.parse_args()
+    ak.verbose = True
+    ak.connect(args.hostname, args.port)
+    ak.shutdown()
