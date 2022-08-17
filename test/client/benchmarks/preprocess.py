20fe4bea0855fee94311bce7a5d31c138b1d41fc
alvaradoo <o.alvarado.rod@outlook.com>
2022-08-17 18:33:04 -0400

    test files modifications

--- test/client/benchmarks/preprocess.py
+++ test/client/benchmarks/preprocess.py
@@ -1,117 +1,144 @@
 #!/usr/bin/env python3                                                         
 
-import time, argparse
-import numpy as np
+import argparse
+import time 
 import arkouda as ak
-import random
-import string
 import arkouda_njit as njit
+import sys
 
-TYPES = ('int64', 'float64', 'bool', 'str')
-
-def time_ak_process_graph():
-    print("Graph Truss Analysis")
+def process_graph(num_edges:int, num_vertices:int, num_cols:int, directed:int, filename:str,\
+                    skipline:int, remap_flag:int, degree_sort_flag:int, rec_flag:int,\
+                    write_flag:int, build_aligned_array_flag:int):
     cfg = ak.get_config()
+    print("Graph Preprocessing -- Single Mode")
     print("server Hostname =",cfg["serverHostname"])
     print("Number of Locales=",cfg["numLocales"])
     print("number of PUs =",cfg["numPUs"])
     print("Max Tasks =",cfg["maxTaskPar"])
     print("Memory =",cfg["physicalMemory"])
-    HomeDir="/rhome/zhihui/"
-    Test1=[ \
-            [28980,5242,2,0,HomeDir+"Adata/SNAP/ca-GrQc.txt.gr"],\
-            [51971,9877,2,0,HomeDir+"Adata/SNAP/ca-HepTh.txt.gr"],\
 
-
-            [88234,4038,2,0,HomeDir+"Adata/SNAP/facebook_combined.txt"],\
-            [106762,26475,3,0,HomeDir+"Adata/SNAP/as-caida20071105.txt.gr"],\
-            [186936,23133,2,0,HomeDir+"Adata/SNAP/ca-CondMat.txt.gr"],\
-            [237010,12008,2,0,HomeDir+"Adata/SNAP/ca-HepPh.txt.gr"],\
-            [367662,36692,2,0,HomeDir+"Adata/SNAP/email-Enron.gr"],\
-            [396160,18772,2,0,HomeDir+"Adata/SNAP/ca-AstroPh.txt.gr"],\
-            [428156,58227,2,0,HomeDir+"Adata/SNAP/loc-brightkite_edges.txt"],\
-            [508837,75879,2,0,HomeDir+"Adata/SNAP/soc-Epinions1.txt"],\
-            [2987624,1134890,2,0,HomeDir+"Adata/SNAP/com-youtube.ungraph.txt.gr"]\
-            [3387388,4033394,2,0,HomeDir+"Adata/SNAP/amazon0601.txt.gr"],\
-
-            [68993773,4847570,2,0,HomeDir+"Adata/SNAP/soc-LiveJournal1.txt"],\
-
-              ]
-    TestMtx=[ [3056,1024,2,0,HomeDir+"Adata/Delaunay/delaunay_n10/delaunay_n10.mtx"],\
-            [6127,2048,2,0,HomeDir+"Adata/Delaunay/delaunay_n11/delaunay_n11.mtx"] ,\
-            [12264, 4096,2,0,HomeDir+"Adata/Delaunay/delaunay_n12/delaunay_n12.mtx"] ,\
-            [24547,8192,2,0,HomeDir+"Adata/Delaunay/delaunay_n13/delaunay_n13.mtx"] ,\
-            [49122,16384,2,0,HomeDir+"Adata/Delaunay/delaunay_n14/delaunay_n14.mtx"] ,\
-            [98274,32768,2,0,HomeDir+"Adata/Delaunay/delaunay_n15/delaunay_n15.mtx"] ,\
-            [196575,65536,2,0,HomeDir+"Adata/Delaunay/delaunay_n16/delaunay_n16.mtx"],\
-
-            [393176,131072,2,0,HomeDir+"Adata/Delaunay/delaunay_n17/delaunay_n17.mtx"],\
-            [786396,262144,2,0,HomeDir+"Adata/Delaunay/delaunay_n18/delaunay_n18.mtx"],\
-            [1572823,524288,2,0,HomeDir+"Adata/Delaunay/delaunay_n19/delaunay_n19.mtx"],\
-            [3145686,1048576,2,0,HomeDir+"Adata/Delaunay/delaunay_n20/delaunay_n20.mtx"],\
-            [6291408,2097152,2,0,HomeDir+"Adata/Delaunay/delaunay_n21/delaunay_n21.mtx"],\
-            [12582869,4194304,2,0,HomeDir+"Adata/Delaunay/delaunay_n22/delaunay_n22.mtx"],\
-            [25165784,8388608,2,0,HomeDir+"Adata/Delaunay/delaunay_n23/delaunay_n23.mtx"],\
-            [50331601,16777216,2,0,HomeDir+"Adata/Delaunay/delaunay_n24/delaunay_n24.mtx"],\
-              ]
-    TestRGG=[ \
-            [14487995,2097152,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_21_s0/rgg_n_2_21_s0.mtx"],\
-            [30359198,4194304,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_22_s0/rgg_n_2_22_s0.mtx"],\
-            [63501393,8388608,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_23_s0/rgg_n_2_23_s0.mtx"],\
-            [132557200,16777216,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_24_s0/rgg_n_2_24_s0.mtx"],\
-              ]
-    TestKron=[ \
-            [2456398,65536,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn16/kron_g500-logn16.mtx"],\
-            [5114375,131072,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn17/kron_g500-logn17.mtx"],\
-            [10583222,262144,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn18/kron_g500-logn18.mtx"],\
-            [21781478,524288,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn19/kron_g500-logn19.mtx"],\
-            [44620272,1048576,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn20/kron_g500-logn20.mtx"],\
-              ]
-
-
-
-    start = time.time()
-    for i in Test1:
-        Edges=i[0]
-        Vertices=i[1]
-        Columns=i[2]
-        Directed=i[3]
-        FileName=i[4]
-        print(Edges,",",Vertices,",",Columns,",",Directed,",",str(FileName))
-        njit.graph_file_preprocessing(Edges,Vertices,Columns,Directed,str(FileName),0,1,0,0,1)
-    for i in TestMtx:
-        Edges=i[0]
-        Vertices=i[1]
-        Columns=i[2]
-        Directed=i[3]
-        FileName=i[4]
-        print(Edges,",",Vertices,",",Columns,",",Directed,",",str(FileName))
-        njit.graph_file_preprocessing(Edges,Vertices,Columns,Directed,str(FileName),1,1,0,0,1)
-    end = time.time()
-    return
-
-
+def process_graphs(num_edges:int, num_vertices:int, num_cols:int, directed:int, dirname:str,\
+                    skipline:int, remap_flag:int, degree_sort_flag:int, rec_flag:int,\
+                    write_flag:int, build_aligned_array_flag:int):
+    cfg = ak.get_config()
+    print("Graph Preprocessing -- Batch Mode")
+    print("server Hostname =",cfg["serverHostname"])
+    print("Number of Locales=",cfg["numLocales"])
+    print("number of PUs =",cfg["numPUs"])
+    print("Max Tasks =",cfg["maxTaskPar"])
+    print("Memory =",cfg["physicalMemory"])
+    
 def create_parser():
-    parser = argparse.ArgumentParser(description="Measure the performance of suffix array building: C= suffix_array(V)")
-    parser.add_argument('hostname', help='Hostname of arkouda server')
-    parser.add_argument('port', type=int, help='Port of arkouda server')
-    parser.add_argument('-v', '--logvertices', type=int, default=5, help='Problem size: log number of vertices')
-    parser.add_argument('-e', '--vedges', type=int, default=2,help='Number of edges per vertex')
-    parser.add_argument('-p', '--possibility', type=float, default=0.01,help='Possibility ')
-    parser.add_argument('-t', '--trials', type=int, default=6, help='Number of times to run the benchmark')
-    parser.add_argument('-m', '--perm', type=int, default=0 , help='if permutation ')
-    parser.add_argument('--numpy', default=False, action='store_true', help='Run the same operation in NumPy to compare performance.')
-    parser.add_argument('--correctness-only', default=False, action='store_true', help='Only check correctness, not performance.')
+    parser = argparse.ArgumentParser(
+        description="Measure the performance of preprocessing a graph."
+    )
+    parser.add_argument("hostname", help="Hostname of arkouda server")
+    parser.add_argument("port", type=int, help="Port of arkouda server")
+    parser.add_argument(
+        "-t", 
+        "--trials", 
+        type=int, 
+        default=10, 
+        help="Number of times to run the benchmark."
+    )
+    parser.add_argument(
+        "--correctness-only", 
+        default=False, 
+        action="store_true", 
+        help="Only check correctness, not performance."
+    )
+    parser.add_argument(
+        "-f",
+        "--filename",
+        type=str,
+        help="Single filename of a graph to preprocess."
+    )
+    parser.add_argument(
+        "--num_edges",
+        type=int,
+        help="Number of edges of a graph. Only to be used out of batch mode!"
+    )
+    parser.add_argument(
+        "--num_vertices",
+        type=int,
+        help="Number of vertices of a graph. Only to be used out of batch mode!"
+    )
+    parser.add_argument(
+        "--num_cols",
+        type=int,
+        help="Number of columns of a graph mtx file. Only to be used out of batch mode!"
+    )
+    parser.add_argument(
+        "--is_directed",
+        default=False,
+        action="store_true",
+        help="Whether or not a graph is directed. Only to be used out of batch mode!"
+    )
+    parser.add_argument(
+        "-d",
+        "--dirname",
+        type=str,
+        help="""Directory name containing multiple files to preprocess (batch method). An extra
+                metadata file must be found in same directory named 'info.txt'. This file must
+                contain number of lines equal to the number of files in the directory. It must 
+                contain the name of the files, the number of edges in a file, the number of 
+                vertices, the number of columns, and 1 if directed, 0 otherwise. The format of each 
+                line must be as follows:
+    
+                name_of_file.mtx       [num_edges]         [num_vertices]      [num_cols]      [0/1]
+                """
+    )
+    parser.add_argument(
+        "--skiplines",
+        type=int,
+        default=0,
+        help="How many lines to skip during file preprocessing.."
+    )
+    parser.add_argument(
+        "--no-remap-flag",
+        default=True,
+        action="store_false",
+        help="Do not remap the vertex IDs that are larger than the total number of vertices."
+    )
+    parser.add_argument(
+        "--degreesort-flag",
+        default=False,
+        action="store_true",
+        help="The smallest vertex ID is the vertex whose degree is the smallest."
+    )
+    parser.add_argument(
+        "--rcm-flag",
+        default=False,
+        action="store_true",
+        help="The RCM algorithm is used to remap the vertex IDs."
+    )
+    parser.add_argument(
+        "--no-write-flag",
+        default=True,
+        action="store_false",
+        help="Do not output the final edge list as a new input file."
+    )
+    parser.add_argument(
+        "--aligned-ary-flag",
+        default=False,
+        action="store_true",
+        help="DO NOT USE NOW! WORK IN PROGRESS!"
+    )
+    
     return parser
 
-
-    
-if __name__ == "__main__":
-    import sys
-    
+if __name__ == "__main__":    
     parser = create_parser()
     args = parser.parse_args()
+    
+    print("Preprocessing graph benchmark.")
     ak.verbose = False
-    print("Preprocessing graph  batch")
     ak.connect(args.hostname, args.port)
-    time_ak_process_graph()
+    
+    if args.filename is not None:
+        process_graph()
+    elif args.dirname is not None:
+        process_graphs()
+    else:
+        print("Error with arguments.")
+        sys.exit(0)
