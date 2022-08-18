8060de18db0f7b8605c11552945b0b95ae1446dd
alvaradoo <o.alvarado.rod@outlook.com>
2022-08-18 16:51:14 -0400

    benchmark file mods

--- test/client/benchmarks/configure.py
+++ test/client/benchmarks/configure.py
@@ -8,40 +8,28 @@ import string
 import arkouda_njit as njit
 
 
-def time_ak_config():
-
+def get_config():
     cfg = ak.get_config()
-
-    print("Number of Locales=",cfg["numLocales"])
-    print("number of PUs =",cfg["numPUs"])
-    print("Max Tasks =",cfg["maxTaskPar"])
-    print("Memory =",cfg["physicalMemory"])
-    print("Version =",cfg["arkoudaVersion"])
-    return
-
+    print("Number of Locales=", cfg["numLocales"])
+    print("number of PUs =", cfg["numPUs"])
+    print("Max Tasks =", cfg["maxTaskPar"])
+    print("Memory =", cfg["physicalMemory"])
+    print("Version =", cfg["arkoudaVersion"])
 
 def create_parser():
-    parser = argparse.ArgumentParser(description="Measure the performance of suffix array building: C= suffix_array(V)")
+    parser = argparse.ArgumentParser(description="Get configuration information of arkouda server.")
     parser.add_argument('hostname', help='Hostname of arkouda server')
     parser.add_argument('port', type=int, help='Port of arkouda server')
-    parser.add_argument('-v', '--logvertices', type=int, default=5, help='Problem size: log number of vertices')
-    parser.add_argument('-e', '--vedges', type=int, default=2,help='Number of edges per vertex')
-    parser.add_argument('-p', '--possibility', type=float, default=0.01,help='Possibility ')
-    parser.add_argument('-t', '--trials', type=int, default=6, help='Number of times to run the benchmark')
-    parser.add_argument('-m', '--perm', type=int, default=0 , help='if permutation ')
-    parser.add_argument('--numpy', default=False, action='store_true', help='Run the same operation in NumPy to compare performance.')
-    parser.add_argument('--correctness-only', default=False, action='store_true', help='Only check correctness, not performance.')
     return parser
-
-
-    
-if __name__ == "__main__":
-    import sys
-    
+  
+if __name__ == "__main__":    
     parser = create_parser()
     args = parser.parse_args()
+
+    print("ARKOUDA CONFIGURATION INFORMATION")
     ak.verbose = False
-    print("Arkouda Configure Information")
     ak.connect(args.hostname, args.port)
-    time_ak_config()
+
+    get_config()
+
     ak.shutdown()
