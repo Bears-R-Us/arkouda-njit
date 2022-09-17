f06c267e9ca199c32b5837ee9f920f7a045f9577
Zhihui Du <zhihuidu@gmail.com>
2022-09-16 20:13:12 -0400

    update parameters

--- server/GenTrussMsg.py
+++ server/GenTrussMsg.py
@@ -3266,10 +3266,10 @@ module TrussMsg {
       var gEdgeDeleted=makeDistArray(Ne,int); //we need a global instead of local array
       var lEdgeDeleted=makeDistArray(Ne,int); //we need a global instead of local array
       var AllRemoved:bool;
-      var BigKRange:int=200;
-      var BigKDividedBy:int=8;
+      var BigKRange:int=100;
+      var BigKDividedBy:int=10;
       var SmallKRange:int=32;
-      var SmallKDividedBy:int=4;
+      var SmallKDividedBy:int=5;
       var ToK:int=100000;
 
       gEdgeDeleted=-1;
