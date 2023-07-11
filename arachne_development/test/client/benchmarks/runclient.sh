d3a40f2487e8848a73f6cecc109bf1a6a16a238a
Zhihui Du <zhihuidu@gmail.com>
2023-07-10 21:59:12 -0400

    update script

--- arachne_development/test/client/benchmarks/runclient.sh
+++ arachne_development/test/client/benchmarks/runclient.sh
@@ -5,5 +5,6 @@ serverport=$3
 numberofnodes=$4
 output="$d-$scriptname-$nodename-$serverport-$numberofnodes-Locales"
 echo "python  $scriptname  $nodename $serverport  2>&1 |tee -a $output-Client"
+cat $scriptname >>$output-Client
 python  $scriptname  $nodename $serverport  2>&1 |tee -a $output-Client
 #python shutdown.py $nodename  $serverport 
