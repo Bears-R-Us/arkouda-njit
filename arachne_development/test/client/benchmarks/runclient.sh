326fbf2bc10d6f210ff037310b92f672c5e83d8f
Zhihui Du <zhihuidu@gmail.com>
2023-07-12 17:45:21 -0400

    update some connected components code (#36)
    * fix bfs bugs
    
    * update batch_cc.py
    
    * add script for CC algorithm
    
    * update preprocessing
    
    * fix rewrite data
    
    * recover skipline in preprocessing.missed for unknow reason
    
    * recover skipline in preprocessing.missed for unknow reason
    
    * modify the parameter of contour
    
    * ake sure changes
    
    * fix read error
    
    * optimize the depth of contour
    
    * update file list
    
    * minor
    
    * update script
    
    * add 1-m-1 algorithm
    
    * write 1m1
    
    * add 1mm and 1m1m algorithm
    
    * check the performance of 11mm and 1m1m
    
    * change position of some script
    
    ---------
    
    Co-authored-by: Oliver Alvarado Rodriguez <41132909+alvaradoo@users.noreply.github.com>

--- arachne_development/test/client/benchmarks/runclient.sh
+++ arachne_development/test/client/benchmarks/runclient.sh
@@ -4,6 +4,7 @@ nodename=$2
 serverport=$3
 numberofnodes=$4
 output="$d-$scriptname-$nodename-$serverport-$numberofnodes-Locales"
-echo "python  $scriptname  $nodename $serverport  2>&1 |tee -a $output-Client"
+echo "python  $scriptname  $nodename $serverport  2>&1 |tee -a $output-Client" >>$output-Client
+cat $scriptname >>$output-Client
 python  $scriptname  $nodename $serverport  2>&1 |tee -a $output-Client
 #python shutdown.py $nodename  $serverport 
