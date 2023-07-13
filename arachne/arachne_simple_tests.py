9b4261a3d20703fcf1b730b3c3e6b4102b0bd6e5
Oliver Alvarado Rodriguez <o.alvarado.rod@outlook.com>
2023-07-13 17:57:51 -0400

    notebook from meeting transfered yesterday, can be used for func

--- arachne/arachne_simple_tests.py
+++ arachne/arachne_simple_tests.py
@@ -19,91 +19,71 @@ if __name__ == "__main__":
     ak.connect(args.hostname, args.port)
 
     # Build graph from an array.
-    src = ak.array([34, 23, 34, 23, 23, 89])
-    dst = ak.array([23, 34, 89, 89, 89, 89])
-    wgt = ak.array([98, 12, 13, .4, 23, 12])
-    graph = ar.PropGraph()
-    graph.add_edges_from(src, dst, wgt)
+    src = ak.array([34, 23, 34, 23, 23, 89, 56, 78, 42, 56, 45, 12, 34, 54, 34, 45, 67, 86, 74, 65])
+    dst = ak.array([23, 34, 89, 89, 89, 89, 78, 23, 34, 23, 23, 34, 17, 13, 11, 45, 89, 89, 34, 89])
+    graph = ar.Graph()
+    print(f"src = {src}")
+    print(f"dst = {dst}\n")
 
-    # Add node labels to the property graph.
-    node_ids = ak.array([23, 34, 89, 89])
-    node_labels = ak.array(["Person", "Person", "Vehicle", "Car"])
-    node_label_dataframe = ak.DataFrame({"nodeIDs" : node_ids, "nodeLabels" : node_labels})
-    graph.add_node_labels(node_label_dataframe)
-    print("Successfully added node labels.")
+    # First step of vertex remapping is to extract the unique keys of an array using GroupBy. 
+    # The GroupBy takes a concatenation of both our src and dst arrays and returns the unique keys 
+    # (vertex names) in sorted order and how many times each key appears.
+    gb_vertices = ak.GroupBy(ak.concatenate([src,dst]))
+    print(f"gb_vertices permutation = {gb_vertices.permutation}")
+    print(f"gb_vertices unique keys = {gb_vertices.unique_keys} with size {gb_vertices.unique_keys.size}")
+    print(f"hb_vertices num groups  = {gb_vertices.ngroups}\n")
 
-    # Add the edge relationships to the property graph.
-    edge_relationships = ak.array(["lives-with", "lives-with", "drives", "drives", "owns", "drives"])
-    edge_relationship_dataframe = ak.DataFrame({"src" : src, "dst" : dst, "edgeRelationships" : edge_relationships})
-    graph.add_edge_relationships(edge_relationship_dataframe)
-    print("Successfully added edge relationships.")
+    # Second step of vertex remapping, we use broadcast to generate the zero-up mapping of the 
+    # vertices.
+    new_vertex_range = ak.arange(gb_vertices.unique_keys.size)
+    gb_vertices_remapped = gb_vertices.broadcast(new_vertex_range)
+    print(f"new_vertex_range     = {new_vertex_range}")
+    print(f"gb_vertices_remapped = {gb_vertices_remapped}\n")
 
-    # Add the node properties to the property graph.
-    # First, we need properties for the nodes labeled as "Person".
-    person_names = ak.array([34, 23])
-    person_property_name = ak.array(["Ann", "Dan"])
-    person_property_born = ak.array(["05-29-1990", "12-05-1975"])
-    # Second, we need properties for the nodes labeled as "Vehicle" and "Car".
-    vehicle_names = ak.array([89])
-    vehicle_property_brand = ak.array(["Tesla"])
-    vehicle_property_model = ak.array(["Model X"])
-    # Now, we send each to a dataframe and add them independently to our property graph.
-    person_node_properties = ak.DataFrame({"nodeIDs" : person_names, "name" : person_property_name, "born" : person_property_born})
-    vehicle_node_properties = ak.DataFrame({"nodeIDs" : vehicle_names, "brand" : vehicle_property_brand, "model" : vehicle_property_model})
-    graph.add_node_properties(person_node_properties)
-    print("Successfully added person node properties.")
-    graph.add_node_properties(vehicle_node_properties)
-    print("Successfully added vehicle node properties.")
+    # Extract out the new edge arrays after they have been remapped. 
+    src_remapped = gb_vertices_remapped[0:src.size]
+    dst_remapped = gb_vertices_remapped[src.size:]
+    print(f"src_remapped = {src_remapped}")
+    print(f"dst_remapped = {dst_remapped}\n")
 
-    # Add the edge properties to the property graphs.
-    # First, we will add the edge properties for the edges with relationship "drives".
-    drives_src = ak.array([34, 23, 89])
-    drives_dst = ak.array([89, 89, 89])
-    drives_property_since = ak.array(["01-10-2011", "01-10-2011", "NULL"])
-    # Second, we will add the edge properties for the edges with relationships "owns".
-    owns_src = ak.array([23])
-    owns_dst = ak.array([89])
-    owns_property_registered = ak.array(["01-10-2011"])
-    # Now, we send each to a dataframe and add them independently to our property graph.
-    drives_edge_properties = ak.DataFrame({"src" : drives_src, "dst" : drives_dst, "since" : drives_property_since})
-    owns_edge_properties = ak.DataFrame({"src" : owns_src, "dst" : owns_dst, "registered" : owns_property_registered})
-    graph.add_edge_properties(drives_edge_properties)
-    print("Successfully added drives edge properties.")
-    graph.add_edge_properties(owns_edge_properties)
-    print("Successfully added owns edge properties.")
-    print()
+    # To see the values that mapped to each other, we can look at the output of arange and of unique
+    # keys from the GroupBy.
+    map_df = ak.DataFrame({"internal" : new_vertex_range, "external" : gb_vertices.unique_keys})
+    print(f"*****MAPPING FROM OLD VERTEX NAME TO NEW VERTEX NAME*****")
+    print(f"{map_df.__repr__}\n")
 
-    # Now, below let's make filters with Arkouda for labels, relationships, and properties.
-    # First, we make filters for all the node labels that contain the value Person.
-    A = ak.arange(0, len(node_label_dataframe), 1)
-    indices = node_label_dataframe["nodeLabels"] == "Person"
-    node_label_filter = node_label_dataframe[indices]["nodeIDs"]
-    print("Nodes with label Person =", node_label_filter)
-    print()
+    # To double check we can do some indexing into gb_vertices unique keys. 
+    print(f"Indexing with 7 should give us 45: {gb_vertices.unique_keys[7]}")
+    print(f"Indexing with 8 should give us 54: {gb_vertices.unique_keys[8]}")
+    print(f"Indexing with 3 should give us 17: {gb_vertices.unique_keys[3]}\n")
 
-    # Second, let's make filters for all the edge relationships that contain the value drives.
-    A = ak.arange(0, len(edge_relationship_dataframe), 1)
-    indices = edge_relationship_dataframe["edgeRelationships"] == "drives"
-    edge_relationship_filter = edge_relationship_dataframe[indices]["src","dst"]
-    print("Edges with relationship drives =\n", edge_relationship_filter.__repr__())
-    print()
+    # Next, we can symmetrize our graph. This means keeping two copies of every edge for when the
+    # graph is undirected. We do this by concatenating our remapped arrays to each other. 
+    src_sym = ak.concatenate([src_remapped,dst_remapped])
+    dst_sym = ak.concatenate([dst_remapped,src_remapped])
+    print(f"*****EDGE LIST AFTER SYMMETRYZING*****")
+    edges_df = ak.DataFrame({"src" : src_sym, "dst" : dst_sym})
+    print(f"{edges_df.__repr__}\n")
 
-    # Third, let's make filters for all the nodes born in December.
-    A = ak.arange(0, len(person_node_properties), 1)
-    indices = person_node_properties["born"].contains("12-")
-    node_property_filter = person_node_properties[indices]["nodeIDs"]
-    print("Nodes with property born in December =", node_property_filter)
-    print()
+    # Now it is time for another GroupBy to sort the edge arrays together. It considers the keys to
+    # to be both edge values at an index i together. This will also handle removing duplicated 
+    # edges.
+    gb_edges = ak.GroupBy([src_sym,dst_sym])
+    print(f"size before GroupBy is {src_sym.size} and after GroupBy it is {gb_edges.unique_keys[0].size}")
+    edges_df = ak.DataFrame({"src" : gb_edges.unique_keys[0], "dst" : gb_edges.unique_keys[1]})
+    print(f"*****EDGE LIST AFTER DEDUPPING*****")
+    print(f"{edges_df.__repr__}\n")
+    src_sym_dedupped = gb_edges.unique_keys[0]
+    dst_sym_dedupped = gb_edges.unique_keys[1]
 
-    # Fourth, and lastly, let's make a filter for all the edges with property since starting in 2011.
-    A = ak.arange(0, len(drives_edge_properties), 1)
-    indices = drives_edge_properties["since"].contains("-2011")
-    edge_property_filter = drives_edge_properties[indices]["src", "dst"]
-    print("Edges with property since from 2011 =\n", edge_property_filter.__repr__())
-    print()
-
-    subgraph = ar.subgraph_view(graph, ar.Graph(), filter_labels=node_label_filter, filter_relationships=edge_relationship_filter, filter_node_properties=node_property_filter, filter_edge_properties=edge_property_filter)
-    print(graph.edges())
-    print(subgraph.edges())
+    # Now, let us get rid of self loops by creating a mask on the indices that are equal to each
+    # other. 
+    loop_mask = src_sym_dedupped == dst_sym_dedupped
+    src_sym_dedupped_delooped = src_sym_dedupped[~loop_mask]
+    dst_sym_dedupped_delooped = dst_sym_dedupped[~loop_mask]
+    print(f"size before delooping is {src_sym_dedupped.size} and after GroupBy it is {src_sym_dedupped_delooped}")
+    print(f"*****EDGE LIST AFTER DELOOPING*****")
+    edges_df = ak.DataFrame({"src" : src_sym_dedupped_delooped, "dst" : dst_sym_dedupped_delooped})
+    print(f"{edges_df.__repr__}\n")
 
     ak.shutdown()
