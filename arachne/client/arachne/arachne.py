4c62183975ef035f8a75ccb599e9edcda929f5af
Oliver Alvarado Rodriguez <o.alvarado.rod@outlook.com>
2023-07-19 17:34:53 -0400

    flipped vertex remap and edge de-dupping/looping, has to be integrated fully

--- arachne/client/arachne/arachne.py
+++ arachne/client/arachne/arachne.py
@@ -205,24 +205,54 @@ class Graph:
         """
         cmd = "addEdgesFrom"
 
-        # First step of vertex remapping is to extract the unique keys of an array using GroupBy. 
-        # The GroupBy takes a concatenation of both our src and dst arrays and returns the unique keys 
-        # (vertex names) in sorted order and how many times each key appears.
-        gb_vertices = ak.GroupBy(ak.concatenate([akarray_src,akarray_dst]))
+        # First, we symmetrize our graph. This means keeping two copies of every edge for when the
+        # graph is undirected. We do this by concatenating our remapped arrays to each other.
+        src_sym = ak.concatenate([akarray_src,akarray_dst])
+        dst_sym = ak.concatenate([akarray_dst,akarray_src])
+        print(f"*****EDGE LIST AFTER SYMMETRYZING*****")
+        edges_df = ak.DataFrame({"src" : src_sym, "dst" : dst_sym})
+        print(f"{edges_df.__repr__}\n")
+
+        # Now, it is time for a GroupBy to sort the edge arrays together. It considers the keys to
+        # to be both edge values at an index i together. This will also handle removing duplicated
+        # edges.
+        gb_edges = ak.GroupBy([src_sym,dst_sym])
+        print(f"size before GroupBy is {src_sym.size} and after GroupBy it is {gb_edges.unique_keys[0].size}")
+        edges_df = ak.DataFrame({"src" : gb_edges.unique_keys[0], "dst" : gb_edges.unique_keys[1]})
+        print(f"*****EDGE LIST AFTER DEDUPPING*****")
+        print(f"{edges_df.__repr__}\n")
+        src_sym_dedupped = gb_edges.unique_keys[0]
+        dst_sym_dedupped = gb_edges.unique_keys[1]
+
+        # Now, let us get rid of self loops by creating a mask on the indices that are equal to each
+        # other.
+        loop_mask = src_sym_dedupped == dst_sym_dedupped
+        src_sym_dedupped_delooped = src_sym_dedupped[~loop_mask]
+        dst_sym_dedupped_delooped = dst_sym_dedupped[~loop_mask]
+        print(f"size before delooping is {src_sym_dedupped.size} and after GroupBy it is {src_sym_dedupped_delooped.size}")
+        print(f"*****EDGE LIST AFTER DELOOPING*****")
+        edges_df = ak.DataFrame({"src" : src_sym_dedupped_delooped, "dst" : dst_sym_dedupped_delooped})
+        print(f"{edges_df.__repr__}\n")
+
+        # Now, we must carry out the vertex remapping by extracting the unique keys of an array
+        # using GroupBy. The GroupBy takes a concatenation of both our src and dst arrays and
+        # returns the unique keys (vertex names) in sorted order and how many times each key
+        # appears.
+        gb_vertices = ak.GroupBy(ak.concatenate([src_sym_dedupped_delooped,dst_sym_dedupped_delooped]))
         print(f"gb_vertices permutation = {gb_vertices.permutation}")
         print(f"gb_vertices unique keys = {gb_vertices.unique_keys} with size {gb_vertices.unique_keys.size}")
         print(f"hb_vertices num groups  = {gb_vertices.ngroups}\n")
 
-        # Second step of vertex remapping, we use broadcast to generate the zero-up mapping of the 
+        # Second step of vertex remapping, we use broadcast to generate the zero-up mapping of the
         # vertices.
         new_vertex_range = ak.arange(gb_vertices.unique_keys.size)
         gb_vertices_remapped = gb_vertices.broadcast(new_vertex_range)
         print(f"new_vertex_range     = {new_vertex_range}")
         print(f"gb_vertices_remapped = {gb_vertices_remapped}\n")
 
-        # Extract out the new edge arrays after they have been remapped. 
-        src_remapped = gb_vertices_remapped[0:akarray_src.size]
-        dst_remapped = gb_vertices_remapped[akarray_src.size:]
+        # Extract out the new edge arrays after they have been remapped.
+        src_remapped = gb_vertices_remapped[0:src_sym_dedupped_delooped.size]
+        dst_remapped = gb_vertices_remapped[src_sym_dedupped_delooped.size:]
         print(f"src_remapped = {src_remapped}")
         print(f"dst_remapped = {dst_remapped}\n")
 
@@ -237,34 +267,8 @@ class Graph:
         print(f"Indexing with 8 should give us 54: {gb_vertices.unique_keys[8]}")
         print(f"Indexing with 3 should give us 17: {gb_vertices.unique_keys[3]}\n")
 
-        # Next, we can symmetrize our graph. This means keeping two copies of every edge for when the
-        # graph is undirected. We do this by concatenating our remapped arrays to each other. 
-        src_sym = ak.concatenate([src_remapped,dst_remapped])
-        dst_sym = ak.concatenate([dst_remapped,src_remapped])
-        print(f"*****EDGE LIST AFTER SYMMETRYZING*****")
-        edges_df = ak.DataFrame({"src" : src_sym, "dst" : dst_sym})
-        print(f"{edges_df.__repr__}\n")
-
-        # Now it is time for another GroupBy to sort the edge arrays together. It considers the keys to
-        # to be both edge values at an index i together. This will also handle removing duplicated 
-        # edges.
-        gb_edges = ak.GroupBy([src_sym,dst_sym])
-        print(f"size before GroupBy is {src_sym.size} and after GroupBy it is {gb_edges.unique_keys[0].size}")
-        edges_df = ak.DataFrame({"src" : gb_edges.unique_keys[0], "dst" : gb_edges.unique_keys[1]})
-        print(f"*****EDGE LIST AFTER DEDUPPING*****")
-        print(f"{edges_df.__repr__}\n")
-        src_sym_dedupped = gb_edges.unique_keys[0]
-        dst_sym_dedupped = gb_edges.unique_keys[1]
-
-        # Now, let us get rid of self loops by creating a mask on the indices that are equal to each
-        # other. 
-        loop_mask = src_sym_dedupped == dst_sym_dedupped
-        src_sym_dedupped_delooped = src_sym_dedupped[~loop_mask]
-        dst_sym_dedupped_delooped = dst_sym_dedupped[~loop_mask]
-        print(f"size before delooping is {src_sym_dedupped.size} and after GroupBy it is {src_sym_dedupped_delooped.size}")
-        print(f"*****EDGE LIST AFTER DELOOPING*****")
-        edges_df = ak.DataFrame({"src" : src_sym_dedupped_delooped, "dst" : dst_sym_dedupped_delooped})
-        print(f"{edges_df.__repr__}\n")
+        self.n_vertices = gb_vertices.unique_keys.size
+        self.n_edges = src_remapped.size
     
     def add_edges_from_chapel_version(  self, akarray_src: pdarray, akarray_dst: pdarray, 
                                         akarray_weight: Union[None, pdarray] = None) -> None:
