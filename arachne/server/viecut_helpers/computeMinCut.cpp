cb004676c81e759130f1bca70357a900e27b576d
Bartosz Marek Bryg <bb474@n0008.cm.cluster>
2026-02-02 17:19:39 -0500

    Updated minimum cut to noi

--- arachne/server/viecut_helpers/computeMinCut.cpp
+++ arachne/server/viecut_helpers/computeMinCut.cpp
@@ -1,14 +1,14 @@
 #include "computeMinCut.h"
-#include <algorithms/global_mincut/cactus/cactus_mincut.h>
+#include <algorithms/global_mincut/noi_minimum_cut.h>
 
-int cpp_computeMinCut(int64_t partition_arr[], int64_t src[], int64_t dst[], int64_t n, int64_t m) {
+int cpp_computeMinCut(int64_t partition_arr[], int64_t src[], int64_t dst[], int64_t n, int64_t m) { 
     int edge_cut_size = -1;
     auto cfg = configuration::getConfig();
     cfg->find_most_balanced_cut = true;
     cfg->threads = 1;
     cfg->save_cut = true;
     cfg->set_node_in_cut = true;
-
+    
     std::shared_ptr<mutable_graph> G = std::make_shared<mutable_graph>();
     G->start_construction(n);
     for(int i = 0; i < n; i++) {
@@ -18,7 +18,7 @@ int cpp_computeMinCut(int64_t partition_arr[], int64_t src[], int64_t dst[], int
 
     for(int j = 0; j < m; j++) {
         int from_node = src[j], to_node = dst[j];
-        int source_node = -1, target_node = -1;
+        int source_node, target_node;
         if(from_node < target_node) {
             source_node = from_node;
             target_node = to_node;
@@ -31,9 +31,9 @@ int cpp_computeMinCut(int64_t partition_arr[], int64_t src[], int64_t dst[], int
     G->finish_construction();
     G->computeDegrees();
     
-    cactus_mincut<std::shared_ptr<mutable_graph>> cmc;
-    edge_cut_size = cmc.perform_minimum_cut(G);
-
+    noi_minimum_cut<std::shared_ptr<mutable_graph>> nmc;
+    edge_cut_size = nmc.perform_minimum_cut(G);
+    
     for(int node_id = 0; node_id < n; node_id++) {
         if(G->getNodeInCut(node_id)) {
             partition_arr[node_id] = 1;
